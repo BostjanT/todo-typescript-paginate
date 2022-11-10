c18ec54ac6f593d7ca4f1ecf47adf497c69183fb
BostjanT <36411197+BostjanT@users.noreply.github.com>
2022-11-10 08:43:55 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Task from "./components/Task";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+      <h1>Task Manager - Do it</h1>
+      <Task />
     </div>
   );
 }
